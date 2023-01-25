6c567de820e6fb8acdf55f971f8948350d5a83a3
KombinatorVit <slonik0775@gmail.com>
2023-01-26 01:08:23 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import {Header} from "./components/Header";
+import {Header} from './layout/Header'
+import {Footer} from './layout/Footer'
+import {Main} from './layout/Main'
 
 function App() {
     return (
         <>
-            <Header/>
+            <Header />
+            <Main />
+            <Footer />
         </>
     );
 }
